d532dffcd27fd8488cda5fea240ceb412ec6de2d
Suraj <gsnv.suraj@gmail.com>
2020-06-30 19:43:52 +0530

    dark mode added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import './bootstrap.min.css';
 
 class App extends React.Component {
 
+  darkMode() {
+    let ele = document.body;
+    ele.classList.toggle('darkMode');
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header p-2">
-          <h1 className="col">ANIME QUIZERR</h1>
+          <h1 className="quizHead">ANIME QUIZERR</h1>
+          <div className="toggle custom-control custom-switch">
+            <input type="checkbox" className="custom-control-input" id="darkSwitch" onClick={this.darkMode} />
+            <label class="custom-control-label" for="darkSwitch">Dark Theme</label>
+          </div>
         </div>
         <div>
           <Main />
