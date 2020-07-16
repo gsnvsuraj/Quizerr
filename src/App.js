865df5651a5d59cd838de0c8838596c15c78dd9d
Suraj <gsnv.suraj@gmail.com>
2020-07-16 14:21:28 +0530

    dark btn fixes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 import Main from './Component/main';
 import './bootstrap.min.css';
 import Navbar from './Component/navbar';
+import DarkMode from './Component/darkmode';
 
 class App extends React.Component {
 
-  
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header p-2">
-        <Navbar />
-        </div>
-        <div className="App-body">
-          <Main />
-        </div>
-      </div>
-    );
-
-  }
+  	render() {
+		return (
+      		<div className="App">
+        		<div className="App-header p-2">
+        			<Navbar />
+        		</div>
+        		
+				<DarkMode />
+        		
+				<div className="App-body">
+          			<Main />
+        		</div>
+      		</div>
+    	);
+	}
 }
 
 export default App;
--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
-import sun from './img/sun.png';
-import moon from './img/moon.png';
 
 class Navbar extends Component {
-
-    darkMode() {
-        let ele = document.body;
-        ele.classList.toggle('darkMode');
     
-        let imgSrc = document.getElementById("imgDark");
-        if (imgSrc.src === sun)
-          imgSrc.src = moon;
-        else
-          imgSrc.src = sun;
-    }
-
     render() { 
         return (
             <nav className="navbar navbar-inverse">
@@ -23,10 +10,11 @@ class Navbar extends Component {
                     <h1 className="quizHead navbar-brand">ANIME OUIZERR</h1>
                 </div>
                 <ul className="nav navbar-nav navbar-right">
-                    <li><img id="imgDark" className="darkImg toggle" src={sun} alt="sun" width="50px" onClick={this.darkMode} /></li>
+                    <li></li>
                 </ul>
                 </div>
-            </nav> 
+            </nav>
+            
         );
     }
 }
